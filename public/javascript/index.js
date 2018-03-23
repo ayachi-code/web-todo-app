1c8ac73db172a33e654ff56b6e1838a360a58292
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-23 14:38:23 +0100

    De P die binnen div binnen todos komt

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -11,7 +11,11 @@ let voeg = document.getElementById("voeg_toe").addEventListener('click',() => {
     //De waarden van de input word opgeslagen in variablen input text
     let waarden = input_van_text = document.getElementById("typen").value;
 
+    //Div word gemaakt dat binnen de todos komt elke keer komt
     let div_binnen_todos = document.createElement("div");
+    //De P die binnen div binnen todos komt
+    let p_binnen_div = document.createElement("p");
+
 
 
 
