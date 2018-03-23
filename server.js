14e9a927462f5991ccb7fd5410c4408bb8e3357c
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-23 13:33:09 +0100

    Server is gelijk aan port 3000 die word gehost

--- server.js
+++ server.js
@@ -2,3 +2,5 @@
 const express = require("express");
 //Variable app is gelijk aan de functie express..
 const app = express();
+//Server is gelijk aan port 3000 die word gehost
+const server = app.listen(3000);
