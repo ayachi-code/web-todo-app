ebbba4e34b3810e7c86f14f10a9ddefc54d5cd0b
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-23 14:33:33 +0100

    variablen waarden is gelijk aan de input value

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,6 +6,6 @@ let input_van_text;
 //Als knop + word geklikt
 let voeg = document.getElementById("voeg_toe").addEventListener('click',() => {
     //De waarden van de input word opgeslagen in variablen input text
-    input_van_text = document.getElementById("typen").value;
+    let waarden = input_van_text = document.getElementById("typen").value;
 
 })
