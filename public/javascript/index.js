cac571de36e857b962eed52b2759595b0c6eed75
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-03-23 14:31:42 +0100

    div gemaakt met class todos

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -5,6 +5,7 @@ let input_van_text;
 
 //Als knop + word geklikt
 let voeg = document.getElementById("voeg_toe").addEventListener('click',() => {
-    //De waarden van de input
+    //De waarden van de input word opgeslagen in variablen input text
     input_van_text = document.getElementById("typen").value;
+
 })
