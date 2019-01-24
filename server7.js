53dccc90e493d0c04c989617b2c0ef099c38dd6c
bilal <bilal.ayachi@hetmml.nl>
2019-01-24 09:26:24 +0000

    s

--- server7.js
+++ server7.js
@@ -3,6 +3,6 @@ const express = require("express");
 //Variable app is gelijk aan de functie express..
 const app = express();
 //Server is gelijk aan port 3000 die word gehost
-const server = app.listen(3000);
+const server = app.listen(9000);
 //public map word gehost...
 app.use(express.static("public"))
